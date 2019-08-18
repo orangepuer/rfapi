d1a1dc8b2ecfb984170dbba5132fdb6ca96d0013
orangepuer <rkhpuer@gmail.com>
2019-08-18 20:15:29 +0300

    Add tests to ArticlesController

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,9 +2,25 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
   describe '#index' do
+    let(:articles) { create_list(:article, 2) }
+    let!(:article) { articles.first }
+    let(:articles_response) { json['data'] }
+    let(:article_response) { articles_response.first['attributes'] }
+
+    before { get :index }
+
     it 'should return success response' do
-      get :index
       expect(response).to have_http_status(:ok)
+    end
+
+    it 'return list of articles' do
+      expect(articles_response.size).to eq(2)
+    end
+
+    %w(title content slug).each do |attribute|
+      it "article object contain #{attribute}" do
+        expect(article_response[attribute]).to eq article.send(attribute)
+      end
     end
   end
 end
