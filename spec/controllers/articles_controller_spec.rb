602ef1253e963ebcf92ab23ad2432fb057234a95
orangepuer <rkhpuer@gmail.com>
2019-08-19 23:21:26 +0300

    Update ArticlesController#index, tests

--- spec/controllers/articles_controller_spec.rb
+++ spec/controllers/articles_controller_spec.rb
@@ -2,10 +2,10 @@ require 'rails_helper'
 
 RSpec.describe ArticlesController, type: :controller do
   describe '#index' do
-    let(:articles) { create_list(:article, 2) }
-    let!(:article) { articles.first }
-    let(:articles_response) { json['data'] }
-    let(:article_response) { articles_response.first['attributes'] }
+    let!(:articles) { create_list(:article, 2) }
+    let(:old_article) { articles.first }
+    let(:newer_article) { articles.last }
+    let(:article_response) { json_data.first['attributes'] }
 
     before { get :index }
 
@@ -14,13 +14,18 @@ RSpec.describe ArticlesController, type: :controller do
     end
 
     it 'return list of articles' do
-      expect(articles_response.size).to eq(2)
+      expect(json_data.size).to eq(2)
     end
 
     %w(title content slug).each do |attribute|
       it "article object contain #{attribute}" do
-        expect(article_response[attribute]).to eq article.send(attribute)
+        expect(article_response[attribute]).to eq newer_article.send(attribute)
       end
+    end
+
+    it 'should return articles in the proper order' do
+      expect(json_data.first['id']).to eq newer_article.id.to_s
+      expect(json_data.last['id']).to eq old_article.id.to_s
     end
   end
 end
--- spec/support/api_helpers.rb
+++ spec/support/api_helpers.rb
@@ -2,4 +2,8 @@ module ApiHelpers
   def json
     @json ||= JSON.parse(response.body)
   end
+
+  def json_data
+    json['data']
+  end
 end
