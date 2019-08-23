c4261b8c512ff1c25734abe00c1eca5ce62cbc31
orangepuer <rkhpuer@gmail.com>
2019-08-23 19:13:57 +0300

    Add UserAuthenticator class

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,8 @@ module Rfapi
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
 
+    config.autoload_paths += [config.root.join('lib')]
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
