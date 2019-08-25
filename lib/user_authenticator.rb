53125411ecca373240acd673d39bfa9ffc2173c5
orangepuer <rkhpuer@gmail.com>
2019-08-25 16:48:33 +0300

    Add the ability to reuse an existing user for authentication

--- lib/user_authenticator.rb
+++ lib/user_authenticator.rb
@@ -9,23 +9,37 @@ class UserAuthenticator
   end
 
   def perform
-    client = Octokit::Client.new(
-                                client_id: ENV['GITHUB_CLIENT_ID'],
-                                client_secret: ENV['GITHUB_CLIENT_SECRET']
-    )
-    token = client.exchange_code_for_token(code)
-
     if token.try(:error).present?
       raise AuthenticationError
     else
-      user_client = Octokit::Client.new(access_token: token)
-      user_data = user_client.user.to_h.slice(:login, :name, :avatar_url, :url)
-
-      @user = User.create(user_data.merge(provider: 'github'))
+      prepare_user
     end
   end
 
   private
 
   attr_reader :code
+
+  def client
+    @client ||= Octokit::Client.new(
+        client_id: ENV['GITHUB_CLIENT_ID'],
+        client_secret: ENV['GITHUB_CLIENT_SECRET']
+    )
+  end
+
+  def token
+    @token ||= client.exchange_code_for_token(code)
+  end
+
+  def user_data
+    @user_data ||= Octokit::Client.new(access_token: token).user.to_h.slice(:login, :name, :avatar_url, :url)
+  end
+
+  def prepare_user
+    if User.exists?(login: user_data[:login])
+      @user = User.find_by(login: user_data[:login])
+    else
+      @user = User.create(user_data.merge(provider: 'github'))
+    end
+  end
 end
