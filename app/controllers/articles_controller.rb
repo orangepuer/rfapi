e8ce1559078b62aa106810667a1691624ea4f007
orangepuer <rkhpuer@gmail.com>
2019-09-09 16:48:26 +0300

    Add ArticlesController#destroy

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,6 +29,14 @@ class ArticlesController < ApplicationController
     render json: article, adapter: :json_api, serializer: ErrorSerializer, status: :unprocessable_entity
   end
 
+  def destroy
+    article = current_user.articles.find(params[:id])
+    article.destroy
+    head :no_content
+  rescue
+    authorization_error
+  end
+
   private
 
   def article_params
