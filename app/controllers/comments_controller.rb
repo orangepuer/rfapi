1de09790cb0c2e791a34bce03695e956f0e58e2b
orangepuer <rkhpuer@gmail.com>
2019-09-11 16:09:11 +0300

    Add CommentsController

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,25 @@
+class CommentsController < ApplicationController
+  skip_before_action :authorize!, only: [:index]
+
+  def index
+    @comments = Comment.all
+
+    render json: @comments
+  end
+
+  def create
+    @comment = Comment.new(comment_params)
+
+    if @comment.save
+      render json: @comment, status: :created, location: @comment
+    else
+      render json: @comment.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+    def comment_params
+      params.require(:comment).permit(:content, :user_id, :article_id)
+    end
+end
