d1d5539a01b41abf1b2414ba020337c303c04892
orangepuer <rkhpuer@gmail.com>
2019-09-28 17:01:07 +0300

    Add RegistrationsController#create

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,5 +1,15 @@
 class RegistrationsController < ApplicationController
-  def create
+  skip_before_action :authorize!, only: :create
 
+  def create
+    user = User.new(registration_params.merge(provider: 'standard'))
+    user.save!
+    render json: user, status: :created
+  end
+
+  private
+
+  def registration_params
+    params.require(:data).require(:attributes).permit(:login, :password) || ActionController::Parameters.new
   end
 end
